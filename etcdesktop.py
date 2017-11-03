a363a4d5ab4fe3dbb0535eb98aa1a574ea15f157
Nando Davis <nandodavis@gmail.com>
2017-11-03 10:01:39 +0100

    Added popover

--- etcdesktop.py
+++ etcdesktop.py
@@ -27,7 +27,7 @@ import os
 
 import gi.repository
 import requests
-from gi.repository import Budgie, GObject, Gtk
+from gi.repository import Budgie, GObject, Gtk, Gdk
 
 gi.require_version('Budgie', '1.0')
 gi.require_version('Wnck', '3.0')
@@ -58,26 +58,62 @@ class etcDesktopApplet(Budgie.Applet):
     """ Budgie.Applet is in fact a Gtk.Bin """
 
     label = None
+    popover = None
 
     def __init__(self, uuid):
         Budgie.Applet.__init__(self)
-
+        self.manager = None
         # Add a button to our UI
+        self.box = Gtk.EventBox()
         self.label = Gtk.Label('etc: ')
         self.label.set_margin_start(6)
         self.label.set_margin_end(6)
-        self.add(self.label)
+        self.box.add(self.label)
+        self.add(self.box)
+
+        self.popover = Budgie.Popover.new(self.box)
+        self.popover.get_style_context().add_class("ETC-popover")
+
+        layout = Gtk.Grid()
+        layout.set_border_width(6)
+
+        self.highlabel = Gtk.Label(_("High(24h): "))
+        layout.attach(self.highlabel, 0, 2, 2, 2)
+        self.lowlabel = Gtk.Label(_("Low(24h): "))
+        layout.attach(self.lowlabel, 0, 4, 2, 2)
+
+        # layout.add(self.lowlabel)
+        self.popover.add(layout)
+        self.popover.get_child().show_all()
         self.show_all()
-        t = myThread(self.label)
+        self.box.connect("button-press-event", self._on_press)
+        t = myThread(self.label, self.highlabel, self.lowlabel)
         t.start()
+    def do_update_popovers(self, manager):
+        """Register popover to manager"""
+        manager.register_popover(self.box, self.popover)
+        self.manager = manager
+
+    def	_on_press(self, box, e):
+        if e.button != 1:
+            return Gdk.EVENT_PROPAGATE
+
+        # Popover show and hide
+        if self.popover.get_visible():
+            self.popover.hide()
+        else:
+            self.manager.show_popover(self.box)
+        return Gdk.EVENT_STOP
+
 
 
 class myThread (threading.Thread):
-    def __init__(self, label):
+    def __init__(self, label, highlabel, lowlabel):
         threading.Thread.__init__(self)
         self.label = label
+        self.highlabel = highlabel
+        self.lowlabel = lowlabel
         dirname, filename = os.path.split(os.path.abspath(__file__))
-        print(dirname)
         self.user_data = json.load(open(dirname + '/settings.json'))
 
     def run(self):
@@ -87,7 +123,13 @@ class myThread (threading.Thread):
                     data_keys = list(data['result'].keys())
                     self.label.set_text("{0}: €{1}".format(
                         self.user_data['name'],
-                        data['result'][data_keys[0]]['b'][0])
+                        data['result'][data_keys[0]]['b'][0][:-5])
+                        )
+                    self.highlabel.set_text("High(24h): €{0}".format(
+                        data['result'][data_keys[0]]['h'][1][:-5])
+                        )
+                    self.lowlabel.set_text("Low(24h): €{0}".format(
+                        data['result'][data_keys[0]]['l'][1][:-5])
                         )
                 else:
                     self.label.set_text("Failed to load data")
